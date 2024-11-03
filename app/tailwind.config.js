e859d6b30bf780b119e19d76abac905329e6f8ea
unknown <dziubekfilip@gmail.com>
2024-11-03 17:08:10 +0100

    linter fixes

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       screens: { 
-        xs: "0px", // matches breakpoints with MUI library 
-        sm: "600px", 
-        md: "900px", 
-        lg: "1200px", 
-        xl: "1536px", 
+        xs: '0px', // matches breakpoints with MUI library 
+        sm: '600px', 
+        md: '900px', 
+        lg: '1200px', 
+        xl: '1536px', 
       },
     },
   },
