bb3d453e2366666b172f3f39010e5b953f32e951
unknown <dziubekfilip@gmail.com>
2024-11-03 09:41:19 +0100

    added linter workflow

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens: {
-        xs: "0px", 
+      screens: { 
+        xs: "0px", // matches breakpoints with MUI library 
         sm: "600px", 
         md: "900px", 
         lg: "1200px", 
